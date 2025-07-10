0e3f0072fe335a54f54ba0f5d87a5fcb4b2fb9f4
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-07-10 10:01:05 +0530

    Delete data/init.py

--- data/init.py
+++
@@ -1,9 +0,0 @@
-import os
-# Path to the data directory
-data_dir = '/content/CbamDenseUnet/data'
-os.makedirs(data_dir, exist_ok=True)
-# Create __init__.py file
-init_path = os.path.join(data_dir, '__init__.py')
-with open(init_path, 'w') as f:
-    f.write("# This makes 'data' a package\n")
-print("✅ __init__.py created at:", init_path)
