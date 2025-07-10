da0c3f4188b1dcb83186ba29a600cdeb3c07f53f
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-07-10 10:28:34 +0530

    Create augmentation.py

---
+++ utils/augmentation.py
@@ -0,0 +1,17 @@
+from torchvision import transforms
+
+def get_train_transforms(img_size=(224, 224)):
+    return transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.2, hue=0.05),
+        transforms.ToTensor()
+    ])
+
+def get_test_transforms(img_size=(224, 224)):
+    return transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.ToTensor()
+    ])
