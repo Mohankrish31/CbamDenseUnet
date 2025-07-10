859219906dcfd5f2bc08613538b1d2115813bf58
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-07-10 11:42:13 +0530

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,10 +1,8 @@
 # /content/CBAMDenseUNet/data/dataset.py
-
 import os
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-
 class PairedDataset(Dataset):
     def __init__(self, low_light_root, normal_light_root, image_size=[200, 200]):
         super().__init__()
@@ -25,16 +23,12 @@ class PairedDataset(Dataset):
             transforms.Resize((image_size[0], image_size[1])),
             transforms.ToTensor()
         ])
-
     def __len__(self):
         return len(self.low_light_dataset)
-
     def __getitem__(self, idx):
         low_img = Image.open(self.low_light_dataset[idx]).convert('RGB')
         normal_img = Image.open(self.normal_light_dataset[idx]).convert('RGB')
         return self.transforms(low_img), self.transforms(normal_img)
-
-
 class UnpairedDataset(Dataset):
     def __init__(self, low_light_root, image_size=[200, 200]):
         super().__init__()
@@ -47,10 +41,8 @@ class UnpairedDataset(Dataset):
             transforms.Resize((image_size[0], image_size[1])),
             transforms.ToTensor()
         ])
-
     def __len__(self):
         return len(self.low_light_dataset)
-
     def __getitem__(self, idx):
         image = Image.open(self.low_light_dataset[idx]).convert('RGB')
         return self.transforms(image), os.path.basename(self.low_light_dataset[idx])
