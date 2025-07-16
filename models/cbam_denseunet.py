3f1410bd7dac47ba8c0748f768abfbbe5e819587
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-07-16 11:18:45 +0530

    Update cbam_denseunet.py

--- models/cbam_denseunet.py
+++ models/cbam_denseunet.py
@@ -1,15 +1,15 @@
 import torch
 import torch.nn as nn
-from models.cbam import cbam
-from models.dense import denseblock
-class cbam_denseunet(nn.Module):
+from models.cbam import CBAM
+from models.dense import Denseblock
+class CBAM_DenseUNet(nn.Module):
     def __init__(self, in_channels=3, base_channels=32):
         super().__init__()
         self.encoder = nn.Sequential(
             nn.Conv2d(in_channels, base_channels, 3, padding=1),
             nn.ReLU(inplace=True),
-            denseblock(base_channels, growth_rate=12, num_layers=3),
-            cbam(base_channels + 3 * 12)
+            Denseblock(base_channels, growth_rate=12, num_layers=3),
+            CBAM(base_channels + 3 * 12)
         )
         self.decoder = nn.Sequential(
             nn.Conv2d(base_channels + 3 * 12, base_channels, 3, padding=1),
