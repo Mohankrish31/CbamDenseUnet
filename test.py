d60fbb48d3ef4364a3f25d68d5878390c6c7311c
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-07-16 17:00:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import torch
 from torch.utils.data import DataLoader
 from models.CBAM_DenseUNet import CBAM_DenseUNet
-from dataset import cvccolondb
+from dataset import cvc-colondb
 from torchvision.utils import save_image
 import os
 import json
