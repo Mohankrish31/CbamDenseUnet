fcb652db7d1d877009013af3d8ce7c4fb7318f51
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-07-16 17:00:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ model = CBAM_DenseUNet(**config["model"]["which_model"]["args"]).cuda()
 model.load_state_dict(torch.load(os.path.join(config["test"]["model_path"], config["test"]["model_name"])))
 model.eval()
 # Test set
-test_data = cvccolondb(**config["test"]["dataset"]["args"])
+test_data = cvc-colondb(**config["test"]["dataset"]["args"])
 test_loader = DataLoader(test_data, **config["test"]["dataloader"]["args"])
 # Create output directory
 os.makedirs(config["test"]["output_images_path"], exist_ok=True)
