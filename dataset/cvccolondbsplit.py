0afccc2775141a0357c350c6179f21ffadb985de
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-07-18 14:13:02 +0530

    Update cvccolondbsplit.py

--- dataset/cvccolondbsplit.py
+++ dataset/cvccolondbsplit.py
@@ -1,21 +1,36 @@
 import os
 from PIL import Image
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision import transforms
-class cvccolondbsplitDataset(Dataset):
-    def __init__(self, low_root, high_root, transform=None):
-        self.low_root = low_root
-        self.high_root = high_root
-        self.transform = transform
-        self.filenames = sorted(os.listdir(low_root))
+class cvccolondbsplit(Dataset):
+    def __init__(self, low_dir, high_dir, transform=True, paired=True):
+        self.low_dir = low_dir
+        self.high_dir = high_dir
+        self.transform_enabled = transform
+        self.paired = paired
+        self.low_images = sorted(os.listdir(low_dir))
+        self.high_images = sorted(os.listdir(high_dir))
+        # Image transforms
+        self.transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ])
     def __len__(self):
-        return len(self.filenames)
+        return min(len(self.low_images), len(self.high_images))
     def __getitem__(self, idx):
-        low_img_path = os.path.join(self.low_root, self.filenames[idx])
-        high_img_path = os.path.join(self.high_root, self.filenames[idx])
-        low = Image.open(low_img_path).convert('RGB')
-        high = Image.open(high_img_path).convert('RGB')
-        if self.transform:
-            low = self.transform(low)
-            high = self.transform(high)
-        return low, high
+        low_path = os.path.join(self.low_dir, self.low_images[idx])
+        high_path = os.path.join(self.high_dir, self.high_images[idx])
+        low_img = Image.open(low_path).convert("RGB")
+        high_img = Image.open(high_path).convert("RGB")
+        if self.transform_enabled:
+            low_img = self.transform(low_img)
+            high_img = self.transform(high_img)
+        else:
+            low_img = transforms.ToTensor()(low_img)
+            high_img = transforms.ToTensor()(high_img)
+        return {
+            'low': low_img,
+            'high': high_img,
+            'low_path': low_path,
+            'high_path': high_path
+        }
