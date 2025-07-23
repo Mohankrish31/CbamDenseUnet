66f3ed4c11f71ca3df523f0f88930782a963fd77
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-07-23 16:12:16 +0530

    Update unet.py

--- models/unet.py
+++ models/unet.py
@@ -1,1 +1,60 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, features=[64, 128, 256, 512]):
+        super(UNet, self).__init__()
+        self.encoder = nn.ModuleList()
+        self.decoder = nn.ModuleList()
+
+        # Encoder
+        for feature in features:
+            self.encoder.append(self._block(in_channels, feature))
+            in_channels = feature
+
+        # Bottleneck
+        self.bottleneck = self._block(features[-1], features[-1]*2)
+
+        # Decoder
+        for feature in reversed(features):
+            self.decoder.append(
+                nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2)
+            )
+            self.decoder.append(self._block(feature*2, feature))
+
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for layer in self.encoder:
+            x = layer(x)
+            skip_connections.append(x)
+            x = F.max_pool2d(x, 2)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for i in range(0, len(self.decoder), 2):
+            x = self.decoder[i](x)
+            skip_connection = skip_connections[i // 2]
+
+            if x.shape != skip_connection.shape:
+                x = F.interpolate(x, size=skip_connection.shape[2:])
+
+            x = torch.cat((skip_connection, x), dim=1)
+            x = self.decoder[i+1](x)
+
+        return self.final_conv(x)
+
+    def _block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
 
