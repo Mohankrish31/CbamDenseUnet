d460018fb94581aec8cf930563068354c968e1ce
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-08-04 20:06:50 +0530

    Update cbam_rdb_block.py

--- models/cbam_rdb_block.py
+++ models/cbam_rdb_block.py
@@ -1,9 +1,1 @@
-class CBAM_RDB_Block(nn.Module):
-    def __init__(self, in_channels, growth_rate=32):
-        super().__init__()
-        self.rdb = RDB(in_channels, growth_rate)
-        self.cbam = CBAM(in_channels)
-    def forward(self, x):
-        out = self.rdb(x)
-        out = self.cbam(out)
-        return out
+
