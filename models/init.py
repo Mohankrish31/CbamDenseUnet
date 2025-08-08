4d804dd29df8aabad43acebd25a7ec7294e8430c
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-08-08 14:12:00 +0530

    Update init.py

--- models/init.py
+++ models/init.py
@@ -4,11 +4,13 @@ from .dense import denseblock
 from .feature_compressor import FeatureCompressor
 from .multiscale_pool import MultiScalePool
 from .enhanced_decoder import EnhancedDecoder
+from .rdb import ResidualDenseBlock  # Added RDB import
 __all__ = [
     "cbam_denseunet",
     "cbam",
     "denseblock",
     "FeatureCompressor",
     "MultiScalePool",
-    "EnhancedDecoder"
+    "EnhancedDecoder",
+    "ResidualDenseBlock"  # Added to exports
 ]
